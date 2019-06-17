7c7af68dad3f2b8785d1fd7fa51ffcc99e3251fd
Shamsuzzaman Sadi <abmsadi06@gmail.com>
2019-06-17 21:44:46 +0200

    added comments

--- include/incAuthentication.php
+++ include/incAuthentication.php
@@ -1,5 +1,5 @@
 <?php
-    function update_basket($product_id, $amount, $user_id) {
+    function update_basket($product_id, $amount, $user_id) { // update basket function
         require 'incDbh.php';
         $sqlquery = "UPDATE tbl_basket SET amount = ? WHERE product_id = ? AND user_id = ? AND order_id IS NULL";
         $stmt = mysqli_stmt_init($connection);
@@ -14,7 +14,7 @@
         }
     }
 
-    function add_basket($product_id, $amount, $user_id) {
+    function add_basket($product_id, $amount, $user_id) { // add basket function
         require 'incDbh.php';
         $sqlquery = "SELECT * FROM tbl_basket WHERE product_id = ? AND user_id = ? AND order_id IS NULL";
         $stmt = mysqli_stmt_init($connection);
@@ -52,51 +52,51 @@
 
   if (isset($_POST['username']) && isset($_POST['password']))
   {
-    require 'incDbh.php';
+    require 'incDbh.php'; // requiring the database
 
     $loginName = $_POST['username'];
     $password = $_POST['password'];
-    if(empty($loginName) || empty($password))
+    if(empty($loginName) || empty($password)) // checking the username and password
     {
-      header("Location: ../index.php?error=emptyFields");
+      header("Location: ../index.php?error=emptyFields"); // show empty field error
       exit();
     }
     else
     {
       $sqlquery = "SELECT * FROM tbl_users WHERE name=? OR email=?";
-      $stmt = mysqli_stmt_init($connection);
-      if(!$stmt->prepare($sqlquery))
+      $stmt = mysqli_stmt_init($connection); //checking the sql connection 
+      if(!$stmt->prepare($sqlquery))  //checking the sql querry
       {
-        header("Location: ../index.php?error=sqlerror");
+        header("Location: ../index.php?error=sqlerror"); //showing the error
         exit();
       }
-      else
+      else 
       {
         $stmt->bind_param("ss", $loginName, $password);
         $stmt->execute();
         $result = $stmt->get_result();
         if($row=$result->fetch_assoc())
         {
-          $pwdCheck = password_verify($password, $row['password']);
+          $pwdCheck = password_verify($password, $row['password']); // checking the password
           if($pwdCheck == false)
           {
-            header("Location: ../index.php?error=wrongpwd");
+            header("Location: ../index.php?error=wrongpwd"); // for wrong password
             exit();
           }
-          elseif($pwdCheck == true)
+          elseif($pwdCheck == true) // when the password is correct move into this block
           {
-            session_start();
+            session_start(); // starting the session
             $user_id = (int) $row['id'];
             $_SESSION['id'] = $user_id;
             $_SESSION['login_time'] = time();
             $_SESSION['name'] = $row['name'];
-            if (count($_COOKIE) > 0) {
+            if (count($_COOKIE) > 0) { 
               for ($i=1; $i < 4; $i++) { 
                   # code...
                   if (isset($_COOKIE['cart_' . $i])) {
                       $quantity = (int) $_COOKIE['cart_' . $i];
                       add_basket($i, $quantity, $user_id);
-                      setcookie('cart_' . $i, '', time() - 3600, '/');
+                      setcookie('cart_' . $i, '', time() - 3600, '/'); //setting up the cookie when user add somthing
                   }
               }
             }
